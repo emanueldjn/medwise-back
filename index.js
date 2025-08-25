df6eb9deb15f5967e467233316eae1b7b1f6f795
Emanuel <emanuelnas29@gmail.com>
2025-08-25 20:27:15 -0300

    cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const { createTables } = require('./createTables');
 createTables();
 
+app.use(cors({
+    origin: 'https://medwise-front.vercel.app',
+    credentials: true
+}));
 app.use(express.json());
 
 // Rotas de usuário
@@ -13,9 +18,9 @@ const userRoutes = require('./src/routes/userRoutes');
 app.use('/api', userRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello word');
+        res.send('Hello word');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+        console.log(`Server is running on port ${port}`);
 });
