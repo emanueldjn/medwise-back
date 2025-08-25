14a93d827486cc28fadfdb93ca51355b8bb33f95
Emanuel <emanuelnas29@gmail.com>
2025-08-25 20:39:47 -0300

    cors v2

--- index.js
+++ index.js
@@ -6,29 +6,16 @@ const port = process.env.PORT || 3000;
 const { createTables } = require('./createTables');
 createTables();
 
-// Configuração CORS
 const corsOptions = {
-    origin: 'https://medwise-front.vercel.app', // origem permitida
-    credentials: true, // permite envio de cookies
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'] // headers permitidos
+    origin: 'https://medwise-front.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 
-// Middlewares
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // trata requisições OPTIONS automaticamente
+app.options('*', cors(corsOptions)); // trata todas as requisições OPTIONS
+
 app.use(express.json());
 
-// Rotas de usuário
-const userRoutes = require('./src/routes/userRoutes');
-app.use('/api', userRoutes);
-
-// Rota de teste
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
-
-// Inicia servidor
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// ...rotas e restante do código...
