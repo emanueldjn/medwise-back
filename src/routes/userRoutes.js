1b8b7fd993d0f2d2bc537593229c909b8d50ad17
Emanuel <emanuelnas29@gmail.com>
2025-08-26 13:56:01 -0300

    removendo upload de fotos v2

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const upload = require('../middlewares/upload');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.delete('/user/:email', userController.delete);
-router.put('/user/:id', upload.single('foto_perfil'), userController.updateUser);
+router.put('/user/:id', userController.updateUser);
 
 module.exports = router;
